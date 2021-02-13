e59dd647a5bdf93ff57c7200008d1d13ad832df9
apsampaio <andre03.sampaio@gmail.com>
2021-02-13 17:27:36 -0300

    add app colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  blue: "#3C7FEA",
+  white: "#F7F5FA",
+  gray: "#CCCCCC",
+  blackPurple: "#32264D",
+  darkGray: "#999999",
+  mediumGray: "#C4C4D1",
+  red: "#DC1637",
+  lightBlue: "#CAD9F6",
+};
+
+export default colors;
