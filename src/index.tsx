c3ecd663cd2d693f9a58214b5ad704e9957e2dd8
apsampaio <andre03.sampaio@gmail.com>
2021-02-18 18:27:23 -0300

    add sign up success page

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import Landing from "./pages/Landing";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SignUpSuccess from "./pages/SignUpSuccess";
 
 const App: React.FC = () => {
-  return <SignUp />;
+  return <SignUpSuccess />;
 };
 
 export default App;
