182a0eb3638e42d77739bea41fcd0ee4cce65a18
apsampaio <andre03.sampaio@gmail.com>
2021-03-02 17:50:30 -0300

    add App Routes

---
+++ src/routes/App.routes.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Landing from "../pages/Landing";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import SignUpSuccess from "../pages/SignUpSuccess";
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => {
+  return (
+    <App.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <App.Screen name="Landing" component={Landing} />
+      <App.Screen name="SignIn" component={SignIn} />
+      <App.Screen name="SignUp" component={SignUp} />
+      <App.Screen name="SignUpSuccess" component={SignUpSuccess} />
+    </App.Navigator>
+  );
+};
+
+export default AppRoutes;
