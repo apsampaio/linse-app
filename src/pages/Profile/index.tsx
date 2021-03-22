7d961e4fabbb87e3b932fe2f4b389afe4c2d2cee
apsampaio <andre03.sampaio@gmail.com>
2021-03-22 19:20:30 -0300

    add exit button

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,29 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
 
-import { Container } from "./styles";
+import ExitIcon from "../../assets/exit.svg";
+
+import {
+  Container,
+  Header,
+  HeaderUp,
+  HeaderExitButton,
+  HeaderExitText,
+} from "./styles";
 
 const Profile: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <></>
+      <Header>
+        <HeaderUp>
+          <HeaderExitButton onPress={() => navigation.navigate("SignIn")}>
+            <ExitIcon />
+            <HeaderExitText>Sair</HeaderExitText>
+          </HeaderExitButton>
+        </HeaderUp>
+      </Header>
     </Container>
   );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,6 +1,31 @@
 import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+import colors from "../../styles/colors";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: tomato;
+  background-color: ${colors.white};
+`;
+
+export const Header = styled.View`
+  height: 30%;
+  padding: 16px 16px;
+  background-color: ${colors.blue};
+
+  justify-content: space-between;
+`;
+
+export const HeaderUp = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const HeaderExitButton = styled(TouchableOpacity)`
+  flex-direction: row;
+`;
+
+export const HeaderExitText = styled.Text`
+  font-size: 18px;
+  margin-left: 8px;
+  color: ${colors.yellow};
 `;
